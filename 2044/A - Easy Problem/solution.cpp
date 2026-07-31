7755788bf2cd692342062cae4d157c13c219f5be
YashMishra9 <anyashmishra@gmail.com>
2026-07-31 23:52:51 +0530

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int count=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<n;j++){
+                if(i+j==n){
+                    count+=1;
+                }
+            }
+        }
+        cout<<count<<endl;
+    }
+ 
+    return 0;
+}
