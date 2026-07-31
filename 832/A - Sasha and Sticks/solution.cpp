aa59c9d177a6a5cfeab71e366c73c9eaf8ccaaf6
YashMishra9 <anyashmishra@gmail.com>
2026-08-01 00:06:34 +0530

    Add Sasha and Sticks [A] from Codeforces

---
+++ 832/A - Sasha and Sticks/solution.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long n,k;
+    cin>>n>>k;
+    if((n/k)%2==1){
+        cout<<"YES";
+    }
+    else cout<<"NO";
+    return 0;
+}
