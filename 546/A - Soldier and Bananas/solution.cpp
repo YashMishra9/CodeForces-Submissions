69022b51cdfe01b03f9f677e6e0afa556e7fa6d5
YashMishra9 <anyashmishra@gmail.com>
2026-08-01 00:03:35 +0530

    Add Soldier and Bananas [A] from Codeforces

---
+++ 546/A - Soldier and Bananas/solution.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long k,n,w;
+    cin>>k>>n>>w;
+    int sum=0;
+    for(int i=1;i<=w;i++){
+        sum+=k*i;
+    }
+    if(sum>n){
+        cout<<sum-n;
+    }
+    else cout<<0;
+ 
+ 
+    return 0;
+}
