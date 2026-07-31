545cb651c85f6850a476d7409678d0c2f6446e2f
YashMishra9 <anyashmishra@gmail.com>
2026-08-01 00:06:39 +0530

    Add New Year and Counting Cards [A] from Codeforces

---
+++ 908/A - New Year and Counting Cards/solution.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int cnt=0;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='o' || s[i]=='i' || s[i]=='u' || s[i]=='1' || s[i]=='3' || s[i]=='5' || s[i]=='7' || s[i]=='9'){
+            cnt+=1;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
