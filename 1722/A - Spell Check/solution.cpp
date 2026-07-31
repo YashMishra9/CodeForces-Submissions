68197c6cc88e799796ec2614dd055cec75c19e1a
YashMishra9 <anyashmishra@gmail.com>
2026-08-01 00:11:10 +0530

    Add Spell Check [A] from Codeforces

---
+++ 1722/A - Spell Check/solution.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;while(t--){
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+       
+ 
+        sort(s.begin(),s.end());
+        if(s=="Timru"){
+            cout<<"YES"<<endl;
+        }
+        else cout<<"NO"<<endl;
+    }
+    return 0;
+}
