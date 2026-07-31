f68d44e16ccbf134f53fdf2feb1bac728f4dc3aa
YashMishra9 <anyashmishra@gmail.com>
2026-08-01 00:02:07 +0530

    Add Mainak and Array [A] from Codeforces

---
+++ 1726/A - Mainak and Array/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+         long long n;
+         cin>>n;
+         vector<long long>arr(n);
+         for(auto &it:arr){
+            cin>>it;
+         }
+         long long ans=arr[n-1]-arr[0];
+         for(int i=1;i<n;i++){
+            ans=max(ans,arr[i]-arr[0]);
+         }
+         for(int i=0;i<n-1;i++){
+            ans=max(ans,arr[n-1]-arr[i]);
+         }
+         for(int i=0;i<n-1;i++){
+            ans=max(ans,arr[i]-arr[i+1]);
+         }
+         cout<<ans<<endl;
+ 
+        }
+ 
+    return 0;
+}
